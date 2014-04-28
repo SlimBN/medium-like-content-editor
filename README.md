12bad09e7783d194a9ae279708f0b77fa2f67078
Suthat Ronglong <suthat@do.in.th>
2014-04-28 16:13:31 +0700

    fix major UI add medium-editor-insert-plugin

--- README.md
+++ README.md
@@ -5,12 +5,12 @@ A web content editor that clones Medium.com 's editor style UI & UX
 
 #### Opensource 
 
-* reset5 
-* Google fonts 
+* Normalzie CSS 
 * Font Awesome
 * Jquery
 * Jquery Elastic
 * daviferreira/medium-editor
+* orthes/medium-editor-insert-plugin
 
 #### Author
 Suthat Ronglong
--- js/script.js
+++ js/script.js
@@ -4,20 +4,22 @@ var editor = new MediumEditor('.editable', {
   buttonLabels: 'fontawesome'
 });
 
-$('#select-photo').on('click', function(e) {
-  e.preventDefault(); 
-  $('#userfile').trigger('click');
+$(function () {
+  $('.editable').mediumInsert({
+    editor: editor,
+    addons: {
+      images: {
+        imagesUploadScript: 'upload.php'
+      }
+    }
+  });
 });
 
 // TEXTAREA
 // $('.content').elastic();
-
-$('.toggle-menu').on('click', function(e) {
+$('#select-photo').on('click', function(e) {
   e.preventDefault(); 
-  $('.entity-menu').fadeIn().show();
-});
-$('.entity-menu').on('mouseleave', function(e) {
-  $('.entity-menu').fadeOut();
+  $('#userfile').trigger('click');
 });
 
 $('#userfile').change(function(){
@@ -31,25 +33,6 @@ function preview(userfile){
         $('.background').attr('style', 'background-image:url(\'' + e.target.result + '\')');
       }
       fileReader.readAsDataURL(userfile.files[0]);
-    }
-  }
-}
-
-$('.choose-image').on('click', function(e) {
-  e.preventDefault(); 
-  $('#file').trigger('click');
-});
-$('#file').change(function(){
-  previewInContent(this);
-});
-function previewInContent(file){
-  if(file.files ){
-    if(file.files[0]){
-      var fileReader = new FileReader();
-      fileReader.onload = function(e){
-        $('.div-editor').append('<br><img src="' + e.target.result + '" class="img" alt=""><br>');
-      }
-      fileReader.readAsDataURL(file.files[0]);
     }
   }
 }
